d55e1689d11b8a918c5333756a49018186ec32c9
TeraWattHour <norman_wiese@icloud.com>
2023-07-27 23:10:15 +0200

    feat: better cli interactions

--- main.go
+++ main.go
@@ -5,13 +5,12 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"path/filepath"
 )
 
 func printUsage() {
-	prompt := "Wrong arguments provided.\n\n" +
+	prompt :=
 		"Usage:\n" +
-		"  pressurise-cli build <project-directory> [<optional-arguments>]"
+			"  pressurise-cli build <project-directory> [<optional-arguments>]"
 
 	fmt.Println(prompt)
 
@@ -26,32 +25,36 @@ func main() {
 	help := flag.Bool("help", false, "a `bool`, whether to show help")
 	args := os.Args[1:]
 
-	if len(args) == 0 {
+	if len(args) < 2 {
 		printUsage()
 		return
 	}
 
 	mode := args[0]
-	if mode == "build" && len(args) >= 2 {
+	args = args[1:]
+	if mode == "build" {
 		noformat := flag.Bool("no-format", false, "a `bool`, whether output file should be formatted with go fmt")
 
-		parseFlags(args[2:])
+		parseFlags(args[1:])
 
 		if *help {
 			printUsage()
 			return
 		}
 
-		projectDirectory := args[1]
+		projectDirectory := args[0]
 
-		if err := buildProject(projectDirectory); err != nil {
-			fmt.Println("project couldn't be built:", err)
+		outputFile, err := buildProject(projectDirectory)
+		if err != nil {
+			fmt.Printf("\033[31m██\033[0m Project couldn't be built:\n%s\n", err)
 			return
 		}
+		fmt.Println("\033[32m██\033[0m Project built successfully")
 
 		if !*noformat {
-			formatGenerated := exec.Command("go", "fmt", filepath.Join(projectDirectory, "pressrelease.go"))
-			formatGenerated.Start()
+			exec.Command("go", "fmt", outputFile).Start()
+		} else {
+			fmt.Println("\033[38;5;208m██\033[0m Skipping formatting")
 		}
 	} else {
 		printUsage()
